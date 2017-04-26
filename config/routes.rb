18890d4390ca9692d9a58c2153f41da7a4488865
Ahya <ayakurdi@gmail.com>
2017-04-26 15:05:37 -0700

    login succesful

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,26 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
   root 'users#index'
   resources :users
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+  get   '/signup' => 'users#new'
 end
+
+=begin
+Prefix Verb         URI Pattern                 Controller#Action
+login_path  GET	     /login		                  sessions#new
+login_path  POST	   /login	         	          sessions#create
+logout_path DELETE	/logout	                   	sessions#destroy
+signup_path GET    /signup                      users#new
+       root GET    /                            users#index
+      users GET    /users(.:format)             users#index
+            POST   /users(.:format)             users#create
+   new_user GET    /users/new(.:format)         users#new
+  edit_user GET    /users/:id/edit(.:format)    users#edit
+       user GET    /users/:id(.:format)         users#show
+            PATCH  /users/:id(.:format)         users#update
+            PUT    /users/:id(.:format)         users#update
+            DELETE /users/:id(.:format)         users#destroy
+=end
